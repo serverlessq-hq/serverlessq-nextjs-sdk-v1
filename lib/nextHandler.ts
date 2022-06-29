75f42d0b63223c14f45dc9177e039508d4a758e2
Alessandro Volpicella <sandro@hashnode.com>
2022-06-29 09:15:53 +0200

    add todos

--- lib/nextHandler.ts
+++ lib/nextHandler.ts
@@ -3,12 +3,22 @@ import { Queue } from './index'
 const VERCEL_URL = process.env.VERCEL_URL
 const IS_VERCEL = process.env.VERCEL
 
+interface Options {
+  urlToOverrideWhenRunningLocalhost: string
+  retries: number
+}
+
+// TODO: Options for retries
+// TODO: Test if handler runs
+// TODO: Test if localhost override works
+
 // @ts-ignore
 export async function QueueHandler(
   nameOfQueue: string,
   route: string,
   // @ts-ignore
-  handler: any
+  handler: any,
+  options: Options
 ) {
   // Create or get queue
   const q = new Queue()
@@ -21,18 +31,19 @@ export async function QueueHandler(
   // @ts-ignore
   async function nextApiHandler(req: any, res: any) {
     console.log('In next api handler')
-    console.log('Request is: ', req)
-    // const response = await quirrel.respondTo(req.body, req.headers)
+    const responseFromHandler = await handler(req, res)
+    console.log('Response from handler: ', responseFromHandler)
     res.status(200)
-    // for (const [header, value] of Object.entries(response.headers)) {
-    //   res.setHeader(header, value)
-    // }
     res.json({ message: 'Done' })
   }
 
   nextApiHandler.enqueue = () => {
     if (!IS_VERCEL) {
       console.log('Not running on Vercel. Probably on localhost')
+      q.enqueue({
+        method: 'GET',
+        target: options.urlToOverrideWhenRunningLocalhost
+      })
       return
     } else {
       q.enqueue({ method: 'GET', target: `https://${VERCEL_URL}/${route}` })
